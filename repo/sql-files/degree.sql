95998a402e83ea5ebf6365a17e25d10887945c87
dawn <9680871+dawn144@user.noreply.gitee.com>
2023-12-17 10:12:35 +0800

    第五次提交

---
+++ repo/sql-files/degree.sql
@@ -0,0 +1,3 @@
+INSERT INTO `degree` (`id`, `degree_name`) VALUES (1, '专科');
+INSERT INTO `degree` (`id`, `degree_name`) VALUES (2, '本科');
+INSERT INTO `degree` (`id`, `degree_name`) VALUES (3, '研究生');
---
+++ repo/sql-files/employees.sql
@@ -0,0 +1,4 @@
+INSERT INTO `employees` (`id`, `emp_name`, `sex`, `age`, `dept_name`, `emp_degree_name`) VALUES (1, '张译', b'0', 25, 1, 2);
+INSERT INTO `employees` (`id`, `emp_name`, `sex`, `age`, `dept_name`, `emp_degree_name`) VALUES (2, '张二', b'1', 26, 2, 3);
+INSERT INTO `employees` (`id`, `emp_name`, `sex`, `age`, `dept_name`, `emp_degree_name`) VALUES (3, '张三', b'1', 27, 3, 3);
+INSERT INTO `employees` (`id`, `emp_name`, `sex`, `age`, `dept_name`, `emp_degree_name`) VALUES (8, '张四', b'0', 28, 1, 1);
